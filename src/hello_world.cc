4886b22855e95874249c1bcd2866d931dc7661f7
Dante Calderon <mail@dantecalderon.dev>
2021-01-30 23:55:47 -0500

    Cpp basic types fibonacci test

--- src/hello_world.cc
+++ src/hello_world.cc
@@ -11,9 +11,26 @@ Napi::String Method(const Napi::CallbackInfo& info) {
   return Napi::String::New(env, "Hello " + static_cast<std::string>(name));
 }
 
+int32_t fib(int32_t x) {
+  return x <= 1 ? x : fib(x - 1) + fib(x - 2);
+}
+
+Napi::Number Fibonacci(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+  Number n =  info[0].As<Number>();
+
+  int32_t result = fib(static_cast<int32_t>(n));
+
+
+  return Napi::Number::New(env, static_cast<double>(result));
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   exports.Set(Napi::String::New(env, "HelloWorld"),
               Napi::Function::New(env, Method));
+  exports.Set(Napi::String::New(env, "Fibonacci"),
+              Napi::Function::New(env, Fibonacci));
   return exports;
 }
 
--- test/test_binding.js
+++ test/test_binding.js
@@ -1,4 +1,5 @@
 const HelloWorld = require("../build/Release/hello-world-native").HelloWorld;
+const Fibonacci = require("../build/Release/hello-world-native").Fibonacci;
 const assert = require("assert");
 
 assert(HelloWorld, "The expected function is undefined");
@@ -10,6 +11,14 @@ function testBasic()
     assert.strictEqual(result, `Hello ${name}`, "Unexpected value returned");
 }
 
+function testFibonacci() {
+    assert.strictEqual(Fibonacci(0), 0, "Fib(0) should be 0")
+    assert.strictEqual(Fibonacci(1), 1, "Fib(1) should be 1")
+    assert.strictEqual(Fibonacci(2), 1, "Fib(2) should be 1")
+    assert.strictEqual(Fibonacci(12), 144, "Fib(12) should be 144")
+}
+
 assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
+assert.doesNotThrow(testFibonacci, undefined, "testFibonacci threw an expection");
 
 console.log("Tests passed- everything looks OK!");
